7fa76bfd1a83f22dd55bc2a8174fd63c0fd71c34
Tu Nguyen <nguy1581@umn.edu>
2022-07-02 01:14:53 -0500

    Created the addToCollection function and tested it with a fake song

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,18 @@
-console.log('***** Music Collection *****')
+console.log("***** Music Collection *****");
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let newRecord = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+
+  collection.push(newRecord);
+
+  return newRecord;
+}
+console.log('Current collection is',collection);
+
+console.log('Most recent addition is',addToCollection('Danger Zone','What',1954));
